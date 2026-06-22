7ee2708f22a01e19e952ed233fc1491307a5d898
xpertauthtools-blip <xpertauth.tools@gmail.com>
2026-06-22 12:37:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 // CONFIGURACIÓN
 // ============================================================
 const SUPABASE_URL      = "https://supabase.xpertauth.com";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzAwMDAwMDAwLCJleHAiOjE5MDAwMDAwMDB9.G-aGMuV36mHVUW5Ifub2wKZhFPf5H-Zntwu2_MOMQBg";
 const sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // ============================================================
